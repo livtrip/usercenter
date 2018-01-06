f1a23e07875fa7217aa431b311c8a4db1e7897f0
mark1xie <545739504@qq.com>
2018-01-06 16:07:33 +0800

    用户中心 init

--- src/main/java/com/trj/usercenter/Application.java
+++ src/main/java/com/trj/usercenter/Application.java
@@ -2,8 +2,9 @@ package com.trj.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SuppressWarnings("deprecation")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application{
 
